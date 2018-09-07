e7dc68364e09a7c32a73e60c29902f8ebb020146
SertesX <danieldoav@gmail.com>
2018-09-07 08:05:36 -0500

    Arbol en cogido hecho

--- src/arbol_genealogico_lab1_datos2/Arbol_Genealogico_Lab1_Datos2.java
+++ src/arbol_genealogico_lab1_datos2/Arbol_Genealogico_Lab1_Datos2.java
@@ -15,7 +15,8 @@ public class Arbol_Genealogico_Lab1_Datos2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArbolG a= new ArbolG();
+        a.setVisible(true);
     }
     
 }
