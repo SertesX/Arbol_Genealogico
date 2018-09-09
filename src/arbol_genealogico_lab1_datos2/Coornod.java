935e8a5b18fc4334ce078c343cd3c23631507a0c
danie <danie@192.168.1.10>
2018-09-09 02:01:33 -0500

    Arbol dibujado faltan relaciones

---
+++ src/arbol_genealogico_lab1_datos2/Coornod.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbol_genealogico_lab1_datos2;
+
+/**
+ *
+ * @author danie
+ */
+public class Coornod {
+    String nombre;
+    int x;
+    int y;
+    
+    public Coornod (String nom,int xx, int yy){
+        nombre=nom;
+        x=xx;
+        y=yy;
+    }
+}
--- src/arbol_genealogico_lab1_datos2/Funciones.java
+++ src/arbol_genealogico_lab1_datos2/Funciones.java
@@ -10,33 +10,36 @@ package arbol_genealogico_lab1_datos2;
  * @author danie
  */
 public class Funciones {
-    
-    Nodo insertar(Nodo raiz, String nom_padre, String nombre){
+
+    Nodo insertar(Nodo raiz, String nom_padre, String nombre) {
         Nodo nuevo = new Nodo(nombre);
-        Nodo padre=buscar(raiz,nom_padre);
-        if(padre.derecho==null){
-            padre.derecho=nuevo;
-        }else{
-            if(padre.izquierdo==null){
-                padre.izquierdo=nuevo;
+        Nodo padre = buscar(raiz, nom_padre);
+        if (padre.derecho == null) {
+            padre.derecho = nuevo;
+        } else {
+            if (padre.izquierdo == null) {
+                padre.izquierdo = nuevo;
             }
         }
         return raiz;
     }
-    
+
     Nodo buscar(Nodo raiz, String nombre) {
-        if (raiz.getNombre().equals(nombre)){ 
-                return raiz; 
-        }else{
-            Nodo izquierdo=buscar(raiz.getIzquierdo(),nombre);
-            Nodo derecho=buscar(raiz.getDerecho(),nombre);
-            if (izquierdo != null){
-                return izquierdo;
-            }
-            if(derecho !=null){
-                return derecho;
+        if (raiz != null) {
+            if (raiz.getNombre().equals(nombre)) {
+                return raiz;
+            } else {
+                Nodo izquierdo = buscar(raiz.getIzquierdo(), nombre);
+                Nodo derecho = buscar(raiz.getDerecho(), nombre);
+                if (izquierdo != null) {
+                    return izquierdo;
+                }
+                if (derecho != null) {
+                    return derecho;
+                }
             }
         }
         return null;
     }
+
 }
